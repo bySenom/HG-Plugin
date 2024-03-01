4584dcf1b5ee714c7d13017a7edc0a9a60d7494c
bySenom <Sashasuperman987@gmail.com>
2024-03-01 14:58:55 +0100

    Linked the scoreboard class with the Main class

--- src/main/java/de/bysenom/hg_plugin/main.java
+++ src/main/java/de/bysenom/hg_plugin/main.java
@@ -4,8 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class main extends JavaPlugin {
 
+    private PlayerScoreboard playerScoreboard;
+
     @Override
     public void onEnable() {
+
+        playerScoreboard = new PlayerScoreboard();
+        getServer().getScheduler().runTaskTimer(this, () -> playerScoreboard.updatePlayerList(), 20L, 20L); // Aktualisiere das Scoreboard alle 20 Ticks (1 Sekunde)
+
         getLogger().info("Das Plugin wurde erfolgreich geladen!"); // Nachricht in der Konsole anzeigen
         getServer().broadcastMessage("Das Plugin wurde erfolgreich geladen!"); // Nachricht im Chat anzeigen
     }
