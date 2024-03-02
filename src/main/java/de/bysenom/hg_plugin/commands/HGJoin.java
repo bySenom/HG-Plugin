b04856c8d05991ed059cfae787eb03cf934175ea
bySenom <Sashasuperman987@gmail.com>
2024-03-02 11:31:34 +0100

    implement the HG Join command

--- src/main/java/de/bysenom/hg_plugin/commands/HGJoin.java
+++ src/main/java/de/bysenom/hg_plugin/commands/HGJoin.java
@@ -1,19 +1,35 @@
 package de.bysenom.hg_plugin.commands;
 
+import de.bysenom.hg_plugin.handlers.LobbyCountdown;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
 public class HGJoin implements CommandExecutor {
+
+    private final LobbyCountdown lobbyCountdown;
+
+    public HGJoin(LobbyCountdown lobbyCountdown) {
+        this.lobbyCountdown = lobbyCountdown;
+    }
+
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String [] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Nur Spieler können diesen Befehl ausführen.");
-            return true;
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!label.equalsIgnoreCase("hgjoin")) {
+            return false; // Not our command, let other plugins handle it
         }
 
-        return false;
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Nur Spieler können diesen Befehl ausführen.");
+            return true; // Command is handled, no need to continue processing
+        }
+
+        Player player = (Player) sender;
+
+        // Start the countdown for the player
+        lobbyCountdown.startCountdown();
+
+        return true; // Command is handled, no need to continue processing
     }
 }
