e43d0aa0f812a4b6859267f7ac9fca0b470aec03
bySenom <Sashasuperman987@gmail.com>
2024-03-13 12:30:42 +0100

    updated the scoreboard visually

--- src/main/java/de/bysenom/hg_plugin/handlers/ScoreBoardHandler.java
+++ src/main/java/de/bysenom/hg_plugin/handlers/ScoreBoardHandler.java
@@ -40,18 +40,22 @@ public class ScoreBoardHandler {
             objective.getScore(" ").setScore(8); // Blank line for spacing
             objective.getScore("").setScore(7); // Blank line
             objective.getScore(" ").setScore(6); // Blank line for spacing
-            objective.getScore("§7Kills:").setScore(5); // Line with text "KILLS:"
+            objective.getScore(ChatColor.BOLD +"Kills§7:").setScore(5); // Line with text "KILLS:"
             objective.getScore("§E%KILLANZAHL%").setScore(4); // Line with placeholder for kill count
             objective.getScore(" ").setScore(3); // Blank line for spacing
-            objective.getScore("").setScore(2); // Blank line
+            objective.getScore("").setScore(-2); // Blank line
             objective.getScore(" ").setScore(1); // Blank line for spacing
-            objective.getScore("§7Players:").setScore(0); // Line with text "Players:"
+            objective.getScore(ChatColor.BOLD +"Spieler§7:").setScore(0); // Line with text "Players:"
+            objective.getScore(ChatColor.BOLD +"Kit§7:").setScore(-3); // Line with text "Players:"
+
+
         }
 
         // Update player count using the objective
         Objective objective = playerObjectives.get(playerId);
         int playerCount = Bukkit.getServer().getOnlinePlayers().size();
-        objective.getScore(String.valueOf(playerCount)).setScore(-1);
+        String coloredPlayerCount = ChatColor.AQUA + String.valueOf(playerCount);
+        objective.getScore(coloredPlayerCount).setScore(-1);
 
         // Update selected kit entry for the player
         String selectedKit = getSelectedKit(player);
